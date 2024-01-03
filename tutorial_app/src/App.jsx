64386cb7c3ace2c6f1b5cf4943f230a822731f2d
Daeyoung Kim <eodudrepublic@gmail.com>
2024-01-03 17:54:29 +0900

    Create (값 생성) 기능 실습
    1. topics를 State로 업그레이드
    2. form 태그로 입력받아 topics에 객체로 추가
    (이때 불변성 고려하는 법 복습)
    3. topics값이 업데이트되면 리렌더링

--- tutorial_app/src/App.jsx
+++ tutorial_app/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'; // App  UI 디자인
 import { useState } from 'react'; // useState : react에서 제공하는 함수 -> 컴포넌트에 상태 변수 추가 (중요!!)
-                                  // -> 컴포넌트의 상태와 생명 주기 기능을 연결한다 해서 Hook이라 한다 카더라 : 개념 참고
 
 function Header(props) {
   console.log('props', props, props.title);
@@ -10,8 +9,6 @@ function Header(props) {
         {/* 여기서의 a 태그는 '유사' HTML임 -> 리액트가 HTML로 변환해주는거기 때문에 문법이 같지 않음! */}
         {/* 그래서 onclick이 아니라 onClick이고, onClick={함수} : a 태그가 클릭됐을때 함수가 호출 */}
         <a href="/" onClick={
-          // function(event) {
-          // 간결하게 arrow function으로 변경
           (event)=>{
             event.preventDefault(); // 원래 a 태그를 클릭했을때 기본적인 기능(이 경우 refresh)을 막음
             props.onChangeMode();   // 막은 원래 기능 대신 props로 전달받은 onChangeMode() 기능 실행
@@ -50,9 +47,6 @@ function Nav(props) {
       </li>
     )
     // lis가 const로 정의되었지만 push가 가능한 이유 : 배열의 이름이 C나 C++처럼 배열의 시작 주소를 의미한다고 생각하면 됨!
-    // GPT : 배열이나 객체 같은 참조 타입(reference type)을 const로 선언하면 그 참조(주소)는 변경할 수 없지만, 참조하는 내용 자체는 변경할 수 있습니다.
-    // GPT : JavaScript에서 const가 변수의 재할당을 막지만, 불변성(immutability)을 보장하지는 않는다는 점을 보여줍니다. 
-    // GPT : 배열과 같은 참조 타입의 경우, 변수가 참조하는 객체의 내용은 const 선언에도 불구하고 변경될 수 있습니다.
   }
   return (
     <nav>
@@ -62,6 +56,7 @@ function Nav(props) {
     </nav>
   );
 }
+
 function Article(props) {
   return (
     <article>
@@ -72,36 +67,60 @@ function Article(props) {
     </article>
   );
 }
+
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    {/* onSubmit : submit 버튼을 눌렀을때 form 태그에서 발생하는 이벤트 */}
+    <form onSubmit={event=>{
+      // 기본 기능 : 페이지가 리로드됨 (http://localhost:3000/?title=test+title&body=test+body)
+      event.preventDefault(); // 기본 기능 막음
+      // event.target : 이벤트가 일어나는 태그 -> 여기서는 form 태그를 가리킴
+      // event.target.title.value : 이벤트가 일어나는 태그에서 'title'이란 태그의 값을 불러옴
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+
+      // props를 통해 onCreate 함수 호출
+      props.onCreate(title, body);
+    }}>
+      <p>
+        {/* text로 title 입력받는 창 */}
+        <input type="text" name="title" placeholder="title"/>
+      </p>
+      {/* title입력창과 body입력창을 다른 줄에 위치시키기 위해 p태그로 감쌈 */}
+      <p>
+        {/* HTML에서 여러 줄을 입력받을땐 textarea */}
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        {/* 폼 데이터를 서버로 전송하는 기능 : submit */}
+        <input type="submit" value="Create" />
+      </p>
+    </form>
+  </article>
+}
 function App() {
-  // const _mode = useState('WELCOME');
   // // State(: [State Variable : 상태 변수, State Setter Function : 상태 업데이트 함수]) = useState(Initial State : 초기 상태);
-  // console.log('_mode', _mode);
-  // const mode = _mode[0];
-  // // State[0] : 상태 변수(State Variable)
-  // const setMode = _mode[1];
-  // // State[1] : 상태 업데이트 함수(State Setter Function)
   const [mode, setMode] = useState('WELCOME');  // 한 줄로 요약 (위랑 내용 같음)
 
   // 실습 : 클릭한 topic 내용을 본문에 표시
   const [id, setId] = useState(null); // 선택한 topic id를 저장하는 state
   
-  // topics 라는 배열 정의 -> 객체 값들을 가짐
-  // 객체(Object) : 객체는 중괄호 {} 안에 0개 이상의 속성(또는 "키-값" 쌍)을 포함할 수 있으며, 각 속성은 키(또는 이름)와 값으로 구성
-  const topics = [
+  // 생성 버튼을 누르면 topics 배열에 새로운 원소를 추가하고, 목록을 업데이트 해야함
+  // -> topics를 state로 업그레이드
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'},
-  ]
+  ]);
+
+  // 다음에 들어갈 원소(객체)의 id를 state로 따로 관리
+  const [nextId, setNextId] = useState(4);  // 초기값 : topics의 마지막 id + 1 -> 4
 
   let content = null;
   if (mode === 'WELCOME') {
     content = <Article title="WELCOME" body="Hello WEB!"></Article>  
   } else if (mode === 'READ') {
-    // // 뭐냐 왜 id가 1이면 html 뭐시기 나와야 하는데 css가 나오지? -> 지금 출력되는 id의 다음게 나오고 있음
-    // // 이런 문제가 발생하는 이유 : 씻팔 대괄호로 접근하면 id가 아니라 index로 접근하니까 그렇지 병신 대영아
-    // console.log('topic', id, topics[id].title, topics[id].body)
-    // content = <Article title={topics[id].title} body={topics[id].body}></Article>  
-
     // 선택한 id 출력
     console.log('id', id)
     for (let i=0; i<topics.length; i++) {
@@ -112,12 +131,33 @@ function App() {
         content = <Article title={topics[i].title} body={topics[i].body}></Article> 
       } 
     }
+  } else if (mode === 'CREATE') {
+    // 입력 폼이 복잡하기 때문에, Create 컴포넌트를 외부에서 정의해 사용
+    content = <Create onCreate={(_title, _body)=>{
+      // onCreate : 생성 버튼을 눌렀을때, 후속 작업을 할 수 있는 인터페이스를 제공
+      console.log('next id', nextId, 'title', _title, 'body', _body);
+
+      // topics에 들어갈 새로운 원소(객체) 생성
+      const newTopic = {id:nextId, title:_title, body:_body}
+
+      // State의 value가 객체 타입일 경우 : 불변성을 고려하여 setValue
+      const newTopics = [...topics] // topics 복제본 생성
+      newTopics.push(newTopic);      // topics의 복제본 수정 : newTopic push
+      setTopics(newTopics);         // 수정한 topics의 복제본을 topics로 set
+
+      // 추가된 topic의 상세 페이지로 넘어가기
+      setId(nextId);
+      setMode('READ');
+
+      // 다음에 들어갈 id값 증가
+      setNextId(nextId+1);
+    }}></Create>
   }
 
   return (
     <div className="App">
       {/* Headet component에 title이라는 속성(prop)을 주고 싶음 -> title의 값이 보이도록*/}
-      <Header title="REACT" onChangeMode={
+      <Header title="WEB" onChangeMode={
         ()=>{
           // 상태 업데이트 함수(setter)를 통해 mode의 값을 'WELCOME'으로 변경
           setMode('WELCOME');
@@ -133,6 +173,12 @@ function App() {
       {/* setMode를 통해 mode의 값이 바뀌면 (State이 바뀌면) mode와 연결된 content 부분이 자동으로 Re-rendering됨! */}
 
       {content} 
+
+      {/* js를 jsx로 바꾸니까 태그 자동완성 작동함!! */}
+      <a href="/create" onClick={event=>{
+        event.preventDefault(); // '/create'로 이동하지 않도록 막음
+        setMode('CREATE');      // mode 상태를 'CREATE'으로 설정
+      }}>Create</a>
     </div>
   );
 }
