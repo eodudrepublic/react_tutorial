a0887e80d820bdcc19d740712cae739f311f7f0b
Daeyoung Kim <eodudrepublic@gmail.com>
2024-01-03 20:25:23 +0900

    Update (값 수정) 기능 실습
    1. 기본값을 넣기 위해 props로 title, body의 값을 컴포넌트에 전달해주는 과정
    2. props로 전달받은 값은 컴포넌트에서 수정 불가능
    3. props로 전달받은 값을 컴포넌트 내에서 수정할 수 있도록 prop을 state로 변환
    4. HTML의 onchange와 React의 onChange의 차이
    5. onChange -> 입력창의 값이 수정될때마다 호출되는 놈을 사용하여 수정된 값을 다시 입력창의 value에 넣어 입력값을 받는(?) 방법 (중요)
    6. 수정된 topic을 만들고, topics의 불변성을 고려해 topics 복제 -> 수정사항 반영 -> topics 업데이트
    7. State들을 사용해 컴포넌트를 깔끔하게 조작

--- tutorial_app/src/App.jsx
+++ tutorial_app/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'; // App  UI 디자인
 import { useState } from 'react'; // useState : react에서 제공하는 함수 -> 컴포넌트에 상태 변수 추가 (중요!!)
 
+// Header component : 헤더
 function Header(props) {
   console.log('props', props, props.title);
   return (
@@ -21,6 +22,7 @@ function Header(props) {
   );
 }
 
+// Nav component : 목차 -> 리스트로 보임
 function Nav(props) {
   const lis = []
   for (let i=0; i<props.topics.length; i++) {
@@ -57,6 +59,7 @@ function Nav(props) {
   );
 }
 
+// Article component : 본문
 function Article(props) {
   return (
     <article>
@@ -68,6 +71,7 @@ function Article(props) {
   );
 }
 
+// Create component : 생성 기능 -> 입력 창
 function Create(props) {
   return <article>
     <h2>Create</h2>
@@ -99,6 +103,52 @@ function Create(props) {
     </form>
   </article>
 }
+
+function Update(props) {
+  // props로 전달받은 값을 컴포넌트 내부에서 수정 가능하도록 state로 변환
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  // 기본 입력 포맷 : Create 코드 참고 -> 수정
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+
+      // Update(submit)버튼을 누르면 onUpdate 함수 호출
+      props.onUpdate(title, body);
+      // props.title, props.body -> 수정 불가능!! => props를 state로 바꿔야한다...?
+    }}>
+      {/* 입력창의 placeholder에 기존 title, body의 값이 들어가 있어야함! */}
+      {/* => props로 title, body의 값을 전달받아 기본 입력값(value)에 넣었음 */}
+
+      {/* 중요 : 기본값으로 넣는 과정, props로 전달받은 값은 수정 못하는 사실, prop을 state로 변환, onChange를 호출해 value값을 업데이트하는 방법 */}
+      <p>
+        {/* update할 title 입력받는 input:text */}
+        <input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+          // 값 입력 -> onChange event 호출 => 그 value를 확인
+          console.log('Update title', event.target.value);
+          // 입력한 값으로 title을 변경
+          setTitle(event.target.value);
+        }}/>
+      </p>
+      <p>
+        {/* update할 body 입력받는 textarea(여러 줄 입력 가능) */}
+        <textarea name="body" placeholder="body" value={body} onChange={event=>{
+          console.log('Update body', event.target.value);
+          // 입력한 값으로 body를 변경
+          setBody(event.target.value);
+        }}></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Update" />
+      </p>
+    </form>
+  </article>
+}
+
 function App() {
   // // State(: [State Variable : 상태 변수, State Setter Function : 상태 업데이트 함수]) = useState(Initial State : 초기 상태);
   const [mode, setMode] = useState('WELCOME');  // 한 줄로 요약 (위랑 내용 같음)
@@ -117,20 +167,32 @@ function App() {
   // 다음에 들어갈 원소(객체)의 id를 state로 따로 관리
   const [nextId, setNextId] = useState(4);  // 초기값 : topics의 마지막 id + 1 -> 4
 
+  // Update 버튼이 mode가 READ일때만 나올 수 있도록하는 지역 변수
+  let contextControl = null;
+
   let content = null;
   if (mode === 'WELCOME') {
     content = <Article title="WELCOME" body="Hello WEB!"></Article>  
   } else if (mode === 'READ') {
     // 선택한 id 출력
     console.log('id', id)
+    let _title, _body = null;
     for (let i=0; i<topics.length; i++) {
       // i번째 객체의 id와 id(state) 값이 같다면 -> i번째 객체의 내용으로 본문 업데이트
       if (topics[i].id === id) {
         // 적용시킬 topic의 내용 확인
         console.log('topic', i, topics[i].title, topics[i].body)
-        content = <Article title={topics[i].title} body={topics[i].body}></Article> 
+        _title = topics[i].title;
+        _body = topics[i].body;
       } 
     }
+    content = <Article title={_title} body={_body}></Article> 
+    
+    // mode가 READ일때만 contextControl 지정 -> Update 보임
+    contextControl = <li><a href={"/update/"+id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
   } else if (mode === 'CREATE') {
     // 입력 폼이 복잡하기 때문에, Create 컴포넌트를 외부에서 정의해 사용
     content = <Create onCreate={(_title, _body)=>{
@@ -151,7 +213,42 @@ function App() {
 
       // 다음에 들어갈 id값 증가
       setNextId(nextId+1);
+
+      // 위처럼, State을 사용하면 컴포넌트를 세련되게 조작할 수 있음
     }}></Create>
+  } else if (mode === 'UPDATE') {
+    // 입력창의 placeholder에 기존 title, body의 값을 넣어주기 위해 찾아서 Update함수의 props로 전달
+    // mode === 'READ' 부분 코드 참고
+    let _title, _body = null;
+    for (let i=0; i<topics.length; i++) {
+      if (topics[i].id === id) {
+        _title = topics[i].title;
+        _body = topics[i].body;
+      } 
+    }
+    // Update 컴포넌트 사용
+    content = <Update title={_title} body={_body} onUpdate={(title, body)=>{
+      // updatedTopic : 수정된 topic
+      // id : update는 기본적으로 'READ' mode일때만 실행되으로 id가 이미 명시되어있음, title, body는 수정한 값을 Update버튼을 눌러 받아옴.
+      const updatedTopic = {id:id, title:title, body:body};
+
+      // 기존 topics 복제 -> newTopics : 불변성 고려
+      const newTopics = [...topics]
+
+      // newTopics에서 일치하는 id의 객체(topic)를 찾아 updatedTopic으로 교체
+      for (let i=0; i<newTopics.length; i++) {
+        if (newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      
+      // newTopics로 topics 업데이트
+      setTopics(newTopics);
+
+      // Update 완료했으면 다시 'READ' mode로
+      setMode('READ');
+    }}></Update>
   }
 
   return (
@@ -173,12 +270,19 @@ function App() {
       {/* setMode를 통해 mode의 값이 바뀌면 (State이 바뀌면) mode와 연결된 content 부분이 자동으로 Re-rendering됨! */}
 
       {content} 
-
+      
       {/* js를 jsx로 바꾸니까 태그 자동완성 작동함!! */}
-      <a href="/create" onClick={event=>{
-        event.preventDefault(); // '/create'로 이동하지 않도록 막음
-        setMode('CREATE');      // mode 상태를 'CREATE'으로 설정
-      }}>Create</a>
+      <ul>
+        <li>
+          {/* Create 기능 실습 */}
+          <a href="/create" onClick={event=>{
+            event.preventDefault(); // '/create'로 이동하지 않도록 막음
+            setMode('CREATE');      // mode 상태를 'CREATE'으로 설정
+          }}>Create</a>
+        </li>
+        {/* mode가 READ일때만 보이는 Update 버튼 (Update 기능 실습) */}
+        {contextControl}
+      </ul>
     </div>
   );
 }
